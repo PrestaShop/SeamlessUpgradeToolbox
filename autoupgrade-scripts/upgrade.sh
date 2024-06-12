b325117bb3378f707de4fe04423bbeaf4df1e412
Thomas Nabord <thomas.nabord@prestashop.com>
2024-06-12 15:52:55 +0100

    Remove last occurence of testCliProcess in upgrade.sh

--- autoupgrade-scripts/upgrade.sh
+++ autoupgrade-scripts/upgrade.sh
@@ -129,7 +129,7 @@ upgrade_process() {
     php modules/autoupgrade/cli-upgrade.php --dir="admin" --action="compareReleases" >>"$LOGS_DIRECTORY"/"$2"_upgrade
 
   docker compose run -u "$DOCKER_USER_ID" --rm -v ./:/var/www/html/ -w /var/www/html/"$RELEASE_DIRECTORY"/"$BASE_VERSION" work-base \
-    php modules/autoupgrade/tests/testCliProcess.php modules/autoupgrade/cli-upgrade.php --dir="admin" >>"$LOGS_DIRECTORY"/"$2"_upgrade
+    php modules/autoupgrade/cli-upgrade.php --dir="admin" >>"$LOGS_DIRECTORY"/"$2"_upgrade
 
   if [ ! $? -eq 0 ]; then
     echo "Upgrade from v$1 to v$2 fail, see" "$LOGS_DIRECTORY"/"$2"_upgrade
